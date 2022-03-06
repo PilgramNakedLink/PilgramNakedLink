1edc6ea4b9c4e682c5ef19b8b2706a00839146be
PilgramNakedLink <85853584+PilgramNakedLink@users.noreply.github.com>
2022-03-06 13:49:34 -0500

    updated online status

--- scripts/loop.sh
+++ scripts/loop.sh
@@ -7,9 +7,9 @@ do
     echo "Writing contents..."  
     ./target/release/tracer export $p | tee ./export/$p.csv > ./output/$p.export
     echo "Uploading data..."
-    curl -F "csvFile=@/Users/pilgram/Documents/pilgram_naked_link/export/$p.csv" 'http://localhost:8888/pilgram/upload.php'
+    curl -F "csvFile=@/Users/pilgram/Documents/Github/pilgram_naked_link/export/$p.csv" 'http://localhost:8888/pilgram/upload.php'
     echo "Uploading trace..."
-    curl -F "traceFile=@/Users/pilgram/Documents/pilgram_naked_link/output/$p.trace" 'http://localhost:8888/pilgram/upload.php'
+    curl -F "traceFile=@/Users/pilgram/Documents/Github/pilgram_naked_link/output/$p.trace" 'http://localhost:8888/pilgram/upload.php'
     echo "\n"
   done <ip-range.txt
 done
